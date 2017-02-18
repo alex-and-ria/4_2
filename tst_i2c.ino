e70bd0e4e298137b8f2384a6255358cdbb129d49
alex-and-ria <alex-and-ria@meta.ua>
2017-02-18 14:50:20 +0200

    18.02.2017/14:50

---
+++ tst_i2c.ino
@@ -0,0 +1,132 @@
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "driverlib/pin_map.h"
+#define scl 19
+#define sda 38
+#define sa 0x21
+#define dl 5
+unsigned long ulPeriod, dutyCycle;
+unsigned long mcs0=0;
+unsigned int state = HIGH;
+
+void setup() {
+    // 40 MHz system clock
+  SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+  ulPeriod = 4;
+  dutyCycle = 2;
+  // Configure PB6 as T0CCP0
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+  GPIOPinConfigure(GPIO_PB6_T0CCP0);
+  GPIOPinTypeTimer(GPIO_PORTB_BASE, GPIO_PIN_6);
+  // Configure timer
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+  TimerConfigure(TIMER0_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM);
+  TimerLoadSet(TIMER0_BASE, TIMER_A, ulPeriod);
+  TimerMatchSet(TIMER0_BASE, TIMER_A, dutyCycle); // PWM
+  TimerEnable(TIMER0_BASE, TIMER_A);
+  Serial.begin(9600);
+  pinMode(scl,OUTPUT);
+  pinMode(sda,OUTPUT);
+  digitalWrite(scl,HIGH);
+  digitalWrite(sda,HIGH);
+  pinMode(GREEN_LED, OUTPUT);
+  mcs0=micros();
+}
+
+void isd(){
+  mcs0=micros();
+  while((micros()-mcs0)<dl);
+}
+
+void sst(){
+  digitalWrite(sda,HIGH); isd();
+  digitalWrite(scl,HIGH); isd();
+  digitalWrite(sda,LOW); isd();
+  digitalWrite(scl,LOW); isd();
+}
+
+void ssp(){
+  digitalWrite(sda,LOW); isd();
+  digitalWrite(scl,HIGH); isd();
+  digitalWrite(sda,HIGH); isd();
+}
+
+void nack(){
+  digitalWrite(sda,HIGH); isd();
+  digitalWrite(scl,HIGH); isd();
+  digitalWrite(scl,LOW); isd();
+  digitalWrite(sda,LOW); isd();
+}
+
+bool sendb(byte data){
+  bool sdd=false;
+  for(byte i=0;i<8;i++){
+    if((data<<i)&0x80)
+      digitalWrite(sda,HIGH);
+    else
+      digitalWrite(sda,LOW);
+    isd();
+    digitalWrite(scl,HIGH); isd();
+    digitalWrite(scl,LOW); isd();
+  }
+  pinMode(sda,INPUT_PULLUP); isd();
+  digitalWrite(scl,HIGH); isd();
+  if(digitalRead(sda)==LOW) sdd=true;
+  else sdd=false;
+  digitalWrite(scl,LOW); isd();
+  pinMode(sda,OUTPUT);
+}
+
+byte getb(){
+  byte data=0;
+  pinMode(sda,INPUT_PULLUP); isd();
+  for(byte i=8;i>0;i--){
+    digitalWrite(scl,HIGH); isd();
+    data<<=1;
+    if(digitalRead(sda)==HIGH) data++;
+    digitalWrite(scl,LOW); isd();
+  }
+  pinMode(sda,OUTPUT);
+  return data;
+}
+
+bool getval(byte regadr, byte& val){
+  sst();
+  if(sendb(2*sa)){
+    if(sendb(regadr)){
+      ssp();
+      sst();
+      if(sendb(2*sa+1)){
+        val=getb();
+        nack(); ssp();
+        return true;
+      }
+      else{
+        ssp(); return false;
+      }
+    }
+    else{
+      ssp(); return false;
+    }
+  }
+  else{
+      ssp(); return false;
+    }
+}
+
+void loop() {
+  byte val=0;
+  if(getval(0x1,val)) {
+    Serial.print(val,HEX);
+    if(val==0x80){
+       digitalWrite(GREEN_LED, state);
+       state=!state;
+       for(unsigned int i=0;i<65535;i++) isd();
+    }
+  }
+  //else Serial.println("qqq");
+  //delay(1000);    
+}
---
+++ tst_ov7670.ino
@@ -0,0 +1,113 @@
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "driverlib/pin_map.h"
+#define sioc 19
+#define siod 38
+#define vsync 2
+#define href 3
+#define pclk 4
+#define d7 5
+#define d6 25
+#define d5 6
+#define d4 26
+#define d3 7
+#define d2 27
+#define d1 8
+#define d0 28
+unsigned long ulPeriod, dutyCycle;
+volatile bool isvsh=false;
+volatile bool ishrr=false;
+volatile bool ishrf=false;
+volatile unsigned int lcnt=0,rcnt=0,lccnt=0;
+unsigned long mcs0=0;
+
+void vsr(){
+  if(digitalRead(vsync)==HIGH){
+    Serial.println(rcnt);
+  }
+  else{
+    rcnt=0;
+  }
+  //isvsh=true;Serial.println(digitalRead(vsync));
+}
+
+void vsf(){isvsh=false; /*Serial.println(rcnt);*/ rcnt=0;}
+
+void hrr(){
+  if(digitalRead(href)==HIGH){
+    rcnt++; lcnt=0;
+  }
+  else{
+    lccnt=lcnt;
+    Serial.println(lccnt);
+  }
+  //ishrr=true; ishrf=false; rcnt++; lcnt=0;
+}
+
+void hrf(){ishrr=false; ishrf=true;}
+
+void pcr(){
+  lcnt++;
+  //if(lcnt==1285) digitalWrite(GREEN_LED,HIGH);
+}
+
+void isd(){
+  mcs0=micros();
+  while((micros()-mcs0)<10);
+}
+
+void setup() {
+  Serial.begin(230400);
+  pinMode(sioc,OUTPUT);
+  pinMode(siod,OUTPUT);
+  digitalWrite(sioc,HIGH);
+  digitalWrite(siod,HIGH);
+  pinMode(vsync,INPUT);
+  pinMode(href,INPUT);
+  pinMode(pclk,INPUT);
+  pinMode(d7,INPUT);
+  pinMode(d6,INPUT);
+  pinMode(d5,INPUT);
+  pinMode(d4,INPUT);
+  pinMode(d3,INPUT);
+  pinMode(d2,INPUT);
+  pinMode(d1,INPUT);
+  pinMode(d0,INPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  // 40 MHz system clock
+  SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+  ulPeriod = 128;
+  dutyCycle =64;
+  // Configure PB6 as T0CCP0
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+  GPIOPinConfigure(GPIO_PB6_T0CCP0);
+  GPIOPinTypeTimer(GPIO_PORTB_BASE, GPIO_PIN_6);
+  // Configure timer
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+  TimerConfigure(TIMER0_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM);
+  TimerLoadSet(TIMER0_BASE, TIMER_A, ulPeriod);
+  TimerMatchSet(TIMER0_BASE, TIMER_A, dutyCycle); // PWM
+  TimerEnable(TIMER0_BASE, TIMER_A);
+  attachInterrupt(vsync,vsr,CHANGE);
+  //attachInterrupt(vsync,vsf,FALLING);
+  attachInterrupt(href,hrr,CHANGE);
+  //attachInterrupt(href,hrf,FALLING);
+  attachInterrupt(pclk,pcr,RISING);
+}
+
+void loop() {
+  /*Serial.print("rcnt="); Serial.println(rcnt);
+  Serial.print("lcnt=");  Serial.println(lcnt);
+  Serial.print("isvsh=");  Serial.println(isvsh);
+  Serial.print("isvsh=");  Serial.println(isvsh);
+  Serial.print("ishrr=");  Serial.println(ishrr);
+  Serial.print("ishrf=");  Serial.println(ishrf); Serial.println();*/
+  //Serial.println(lcnt);
+  //Serial.println(isvsh);
+  //Serial.println(isvsh);
+  //Serial.println(ishrr);
+  //Serial.println(ishrf);
+}
