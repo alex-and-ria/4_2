9f9461da2d5d9b7f6678a9178601c1e5fa34c58d
alex-and-ria <alex-and-ria@meta.ua>
2017-02-26 23:17:52 +0200

    26.02.2017/23:17

--- pg.cpp
+++ pg.cpp
@@ -1,10 +1,21 @@
-#pragma comment(lib, "ws2_32.lib")
+
 #include <iostream>
 #include <ctime>
 #include <chrono>
 #include<string>
+#ifdef _WIN32
+#pragma comment(lib, "ws2_32.lib")
 #include <winsock2.h>
 #include <windows.h>
+#include <ws2tcpip.h>
+#elif __linux__
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <cstring>
+#endif
 
 using namespace std;
 typedef unsigned short int usi;
@@ -30,15 +41,13 @@ struct IPheader{
     unsigned int	nDestAddr;
 };
 
-unsigned short CalcChecksum (char *pBuffer, int nLen){
-	//Checksum for ICMP is calculated in the same way as for
-	//IP header
+unsigned short CalcChecksum (unsigned char *pBuffer, int nLen){
+	//Checksum for ICMP is calculated in the same way as for IP header
 	//This code was taken from: http://www.netfor2.com/ipsum.htm
 	unsigned short nWord;
 	unsigned int nSum = 0;
 	int i;
-	//Make 16 bit words out of every two adjacent 8 bit words in the packet
-	//and add them up
+	//Make 16 bit words out of every two adjacent 8 bit words in the packet and add them up
 	for (i = 0; i < nLen; i = i + 2)	{
 		nWord =((pBuffer [i] << 8)& 0xFF00) + (pBuffer [i + 1] & 0xFF);
 		nSum = nSum + (unsigned int)nWord;
@@ -52,13 +61,11 @@ unsigned short CalcChecksum (char *pBuffer, int nLen){
 	return ((unsigned short) nSum);
 }
 
-bool ValidateChecksum (char *pBuffer, int nLen){
+bool ValidateChecksum (unsigned char *pBuffer, int nLen){
 	unsigned short nWord;
 	unsigned int nSum = 0;
 	int i;
-
-	//Make 16 bit words out of every two adjacent 8 bit words in the packet
-	//and add them up
+	//Make 16 bit words out of every two adjacent 8 bit words in the packet and add them up
 	for (i = 0; i < nLen; i = i + 2)	{
 		nWord =((pBuffer [i] << 8)& 0xFF00) + (pBuffer [i + 1] & 0xFF);
 		nSum = nSum + (unsigned int)nWord;
@@ -67,41 +74,38 @@ bool ValidateChecksum (char *pBuffer, int nLen){
 	while (nSum >> 16)	{
 		nSum = (nSum & 0xFFFF) + (nSum >> 16);
 	}
-	//To validate the checksum on the received message we don't complement the sum
-	//of one's complement
-	//One's complement the result
-	//nSum = ~nSum;
+	//To validate the checksum on the received message we don't complement the sum of one's complement
+	//One's complement the result nSum = ~nSum;
 	//The sum of one's complement should be 0xFFFF
 	return ((unsigned short)nSum == 0xFFFF);
 }
-
+#ifdef _WIN32
 bool Initialize (){
-	//Initialize WinSock
-	WSADATA wsaData;
+	WSADATA wsaData;//Initialize WinSock
 	if (WSAStartup (MAKEWORD (2, 2), &wsaData) == SOCKET_ERROR)	{
 		cerr << endl << "An error occured in WSAStartup operation: " << "WSAGetLastError () = " << WSAGetLastError () << endl;
 		return false;
 	}
-	/*SYSTEMTIME time;
-	::GetSystemTime (&time);*/
-	//Seed the random number generator with current millisecond value
-	srand (time(NULL));
+	srand(time(NULL)); //Seed the random number generator
 	return true;
 }
 
-bool UnInitialize (){
-	//Cleanup
-	if (WSACleanup () == SOCKET_ERROR)	{
+bool UnInitialize (){	
+	if (WSACleanup () == SOCKET_ERROR)	{//Cleanup
 		cerr << endl << "An error occured in WSACleanup operation: WSAGetLastError () = " << WSAGetLastError () << endl;
 		return false;
 	}
 	return true;
 }
+#endif
 
 bool ResolveIP (string& szRemoteHost, string& szIPAddress){
 	hostent *pHostent = gethostbyname (szRemoteHost.c_str());
 	if (pHostent == NULL){
-		cerr << endl << "An error occured in gethostbyname operation: WSAGetLastError () = " << WSAGetLastError () << endl;
+		cerr << endl << "An error occured in gethostbyname operation: WSAGetLastError () = ";
+		#ifdef _WIN32
+		 cout<< WSAGetLastError () << endl;
+		#endif
 		return false;
 	}
 	szIPAddress=string(pHostent->h_addr_list[0]);
@@ -118,23 +122,22 @@ void PrintUsage (){
 }
 
 int main(int argc, char** argv){
-	if (argc < 2 || argc > 5){
+	if (argc > 5){
 		PrintUsage ();
 		return 0;
 	}
+	#ifdef _WIN32
 	if (Initialize () == false){
 		return -1;
 	}
+	#endif
 	int nSequence = 0;
 	int nTimeOut = 5000;	//Request time out for echo request (in milliseconds)
 	int nMessageSize = 32;	//The message size that the ICMP echo request should carry with it
 	int nCount = 4;	//Number of times the request has to be send
-	string szRemoteIP, szRemoteHost(argv [1]); char* SendBuffer=NULL;
-	/*cout<<"\nremoteHost="<<szRemoteHost;
-	cout<<"\n\n";
-	for(usi i=0;i<argc;i++){
-		cout<<"\ni="<<i<<' '<<string(argv[i])<<'\n';
-	}*/
+	string szRemoteIP, szRemoteHost;
+	if(argc>=2) szRemoteHost=string(argv [1]); else szRemoteHost=string("www.google.com");
+	char* SendBuffer=NULL;
 	//get param;
 	switch(argc){
 		case 5:
@@ -154,158 +157,119 @@ int main(int argc, char** argv){
 		return -1;
 	}
 	in_addr in; in.s_addr = *(u_long *) szRemoteIP.c_str();
-	cout << "Pinging " << szRemoteHost << " [" << 
-	(usi)in.S_un.S_un_b.s_b1<<'.'<<(usi)in.S_un.S_un_b.s_b2<<'.'<<(usi)in.S_un.S_un_b.s_b3<<'.'<<(usi)in.S_un.S_un_b.s_b4
-	<< "] with " << nMessageSize << " bytes of data\n";
-	
+	cout << "\nPinging " << szRemoteHost << " [";
+	#ifdef _WIN32
+	cout<<(usi)in.S_un.S_un_b.s_b1<<'.'<<(usi)in.S_un.S_un_b.s_b2<<'.'<<(usi)in.S_un.S_un_b.s_b3<<'.'<<(usi)in.S_un.S_un_b.s_b4;
+	#elif __linux__
+	cout<<inet_ntoa(in);
+	#endif
+	cout<< "] with " << nMessageSize << " bytes of data\n";
 	//create socet;
 	ICMPheader sendHdr; sendHdr.nId = htons (rand ());	//Set the transaction Id
-	SOCKET sock= socket (AF_INET, SOCK_RAW, IPPROTO_ICMP);	//Create a raw socket which will use ICMP
-	sockaddr_in dest;	//Dest address to send the ICMP request
-	dest.sin_addr.S_un.S_addr = inet_addr (szRemoteIP.c_str());
+	#ifdef _WIN32
+	SOCKET sock;
+	#elif __linux__
+	int sock;
+	#endif
+	sock= socket (AF_INET, SOCK_RAW, IPPROTO_ICMP);	//Create a raw socket which will use ICMP
+	sockaddr_in dest; memset(&dest, 0, sizeof(dest));//Dest address to send the ICMP request
+	memcpy(&(dest.sin_addr), szRemoteIP.c_str(),sizeof(szRemoteIP.c_str()));//dest.sin_addr.S_un.S_addr = inet_addr (szRemoteIP.c_str());
 	dest.sin_family = AF_INET;
-	dest.sin_port = rand ();	//Pick a random port
 
-	int nResult = 0;
-	fd_set fdRead;
-	SYSTEMTIME timeSend, timeRecv;//TODO;
-	int nTotalRoundTripTime = 0, nMaxRoundTripTime = 0, nMinRoundTripTime = -1, nRoundTripTime = 0;
+	int nResult = 0; chrono::time_point<std::chrono::system_clock> tsend, trecv;
+	//fd_set fdRead; SYSTEMTIME timeSend, timeRecv;//TODO;
+	int nTotalRoundTripTime = 0, nMaxRoundTripTime = 0, nMinRoundTripTime = -1;
 	int nPacketsSent = 0, nPacketsReceived = 0;
-	timeval timeInterval = {0, 0};
-	timeInterval.tv_usec = nTimeOut * 1000;
-	//while (nPacketsSent < nCount){
+	//timeval timeInterval = {0, 0}; timeInterval.tv_usec = nTimeOut * 1000;
+	while (nPacketsSent < nCount){
 		//create ICMP message;
 		SendBuffer = new char [sizeof (ICMPheader) + nMessageSize];
 		sendHdr.byType = 8;	//Eight for ICMP echo message
 		sendHdr.byCode = 0;	//Zero for ICMP echo and reply messages
 		sendHdr.nChecksum = 0;	//Checksum is calculated later on
 		sendHdr.nSequence = htons (nSequence++);
-		/*cout<<"\nb\n";
-		for(usi i=0;i<sizeof (ICMPheader);i++){
-			cout<<" SendBuffer["<<i<<"]="<<SendBuffer[i];
-		}
-		cout<<"\n\n sendHdr.byType="<<(usi)sendHdr.byType<<" sendHdr.byCode="<<(usi)sendHdr.byCode<<" sendHdr.nChecksum="
-		<<(usi)sendHdr.nChecksum<<" sendHdr.nId="<<(usi)sendHdr.nId<<" sendHdr.nSequence="<<(usi)sendHdr.nSequence<<"\n\na\n";*/
-		for(usi i=0;i<sizeof (ICMPheader);i++){
-			SendBuffer[i]=((unsigned char*)&sendHdr)[i];
-		}
-		/*for(usi i=0;i<sizeof (ICMPheader);i++){
-			cout<<" SendBuffer["<<i<<"]="<<(usi)SendBuffer[i];
-		}*/
-		for(unsigned int i=sizeof (ICMPheader);i<sizeof (ICMPheader) + nMessageSize;i++){
-			SendBuffer[i]=(unsigned char)0;
-		}
+		memcpy(SendBuffer,&sendHdr,sizeof(ICMPheader));
+		memset(SendBuffer+sizeof (ICMPheader),0,nMessageSize);
 		//Calculate checksum over ICMP header and message data
-		sendHdr.nChecksum = htons (CalcChecksum (SendBuffer, sizeof (ICMPheader) + nMessageSize));	
-		for(usi i=0;i<sizeof (ICMPheader);i++){
-			SendBuffer[i]=((unsigned char*)&sendHdr)[i];
-		}
+		sendHdr.nChecksum = htons (CalcChecksum ((unsigned char*)SendBuffer, sizeof (ICMPheader) + nMessageSize));
+		memcpy(SendBuffer,&sendHdr,sizeof(ICMPheader));
 		//send;
-		nResult = sendto (sock, SendBuffer, sizeof (ICMPheader) + nMessageSize, 0, (SOCKADDR *)&dest, sizeof (SOCKADDR_IN));
-		::GetSystemTime (&timeSend);//TODO;
+		tsend=chrono::system_clock::now();
+		nResult = sendto (sock, SendBuffer, sizeof (ICMPheader) + nMessageSize, 0, (struct sockaddr*)&dest, sizeof (sockaddr_in));		
 		nPacketsSent++;
-		if (nResult == SOCKET_ERROR){
-			cerr << endl << "An error occured in sendto operation: " << "WSAGetLastError () = " << WSAGetLastError () << endl;
-			UnInitialize ();
+		#ifdef _WIN32
+			auto sckerr=SOCKET_ERROR;
+		#elif __linux__
+			auto sckerr=-1;
+		#endif
+		if (nResult == sckerr){
+			cerr << endl << "An error occured in sendto operation: " << "WSAGetLastError () = ";
+		#ifdef _WIN32
+		 cout<< WSAGetLastError () << endl; UnInitialize ();
+		#endif
 			delete []SendBuffer;
 			return -1;
-		}
-		FD_ZERO (&fdRead);//TODO;
-		FD_SET (sock, &fdRead);
-		if ((nResult = select (0, &fdRead, NULL, NULL, &timeInterval))== SOCKET_ERROR){
-			cerr<<"\nAn error occured in select operation: "<<"WSAGetLastError="<<WSAGetLastError ()<<'\n';
-			delete []pSendBuffer;
-			return -1;
-		}//TODO;
+		}		
 		//recieve;
-		if (nResult>0 && FD_ISSET(sock, &fdRead)){
-			char *RecvBuffer = new char [1500];//Allocate a large buffer to store the response
-			if ((nResult = recvfrom (sock, RecvBuffer, 1500, 0, 0, 0))== SOCKET_ERROR){
-				cerr<<"\nAn error occured in recvfrom operation: " << "WSAGetLastError () = "<<WSAGetLastError()<<'\n';
-				UnInitialize ();
-				delete []pSendBuffer;
-				delete []pRecvBuffer;
+		chrono::duration<double,milli> ds=chrono::system_clock::now()-tsend;
+		if (nResult>0 && ds.count()<=((double)nTimeOut)){
+			unsigned char *RecvBuffer = new unsigned char [1500];//Allocate a large buffer to store the response
+			if ((nResult = recvfrom (sock, (char*)RecvBuffer, 1500, 0, 0, 0))== sckerr){
+				cerr<<"\nAn error occured in recvfrom operation: " << "WSAGetLastError () = ";
+		#ifdef _WIN32
+		 cout<< WSAGetLastError () << endl; UnInitialize ();
+		#endif
+				delete []SendBuffer;
+				delete []RecvBuffer;
 				return -1;
 			}
-			::GetSystemTime (&timeRecv);//TODO;//Get the time at which response is received
+			trecv=chrono::system_clock::now();//::GetSystemTime (&timeRecv);//TODO;//Get the time at which response is received
 			ICMPheader recvHdr;//We got a response so we construct the ICMP header and message out of it
-			char *ICMPbuffer = NULL;
-			ICMPbuffer = RecvBuffer + sizeof(IPheader);//The response includes the IP header as well,
+			IPheader ipHdr;
+			unsigned char *ICMPbuffer= RecvBuffer + sizeof(IPheader);//The response includes the IP header as well,
 			//so we move 20 bytes ahead to read the ICMP header
 			int nICMPmsgLen = nResult - sizeof(IPheader); //ICMP message length is calculated by subtracting the IP header size
 			//from the total bytes received
-//////////////////////////////////////////
-			//Construct the ICMP header
-			memcpy_s (&recvHdr, sizeof (recvHdr), ICMPbuffer, sizeof (recvHdr));
-
-			//Construct the IP header from the response
-			IPheader ipHdr;
-			memcpy_s (&ipHdr, sizeof (ipHdr), RecvBuffer, sizeof (ipHdr));
-
-			recvHdr.nId = recvHdr.nId;
-			recvHdr.nSequence = recvHdr.nSequence;
+			memcpy(&recvHdr,ICMPbuffer,sizeof(ICMPheader));//Construct the ICMP header
+			memcpy(&ipHdr,RecvBuffer,sizeof(IPheader));//Construct the IP header from the response
 			recvHdr.nChecksum = ntohs (recvHdr.nChecksum);
-
-			//Check if the response is an echo reply, transaction ID and sequence number are same
-			//as for the request, and that the checksum is correct
-			if (recvHdr.byType == 0 &&
-				recvHdr.nId == sendHdr.nId &&
-				recvHdr.nSequence == sendHdr.nSequence &&
-				ValidateChecksum (pICMPbuffer, nICMPMsgLen)  && 
-				memcmp (pSendBuffer + sizeof(ICMPheader), pRecvBuffer + sizeof (ICMPheader) + sizeof(IPheader), 
-					nResult - sizeof (ICMPheader) - sizeof(IPheader)) == 0)
-			{
-				//All's OK
-				int nSec = timeRecv.wSecond - timeSend.wSecond;
-				if (nSec < 0)
-				{
+			//check			
+			if (recvHdr.byType == 0 &&//Check if the response is an echo reply, transaction ID and sequence number are same
+			recvHdr.nId == sendHdr.nId && recvHdr.nSequence == sendHdr.nSequence &&
+			ValidateChecksum (ICMPbuffer, nICMPmsgLen)  && //as for the request, and that the checksum is correct
+			memcmp(SendBuffer+sizeof(ICMPheader),RecvBuffer+sizeof(ICMPheader)+sizeof(IPheader),
+			nResult-sizeof(ICMPheader)-sizeof(IPheader))==0){//All's OK
+				/*int nSec = timeRecv.wSecond - timeSend.wSecond;//TODO;
+				if (nSec < 0){
 					nSec = nSec + 60;
+				}*/
+				//int nMilliSec = abs (timeRecv.wMilliseconds - timeSend.wMilliseconds);
+				chrono::duration<double,milli> rtt=trecv-tsend;
+				//int nRoundTripTime=abs (nSec * 1000 - nMilliSec);
+				cout << "Reply " << nPacketsReceived << ": bytes = " << nResult - sizeof (ICMPheader) - sizeof (IPheader) 
+					<< ", time = " << rtt.count() << "ms, TTL = " << (int)ipHdr.byTtl << endl;
+
+				nTotalRoundTripTime = nTotalRoundTripTime + rtt.count();
+				if (nMinRoundTripTime == -1){
+					nMinRoundTripTime = rtt.count();;
+					nMaxRoundTripTime = rtt.count();;
 				}
-
-				int nMilliSec = abs (timeRecv.wMilliseconds - timeSend.wMilliseconds);
-
-				int nRoundTripTime = 0;
-				nRoundTripTime = abs (nSec * 1000 - nMilliSec);
-
-				cout << "Reply from " << pszRemoteIP << ": bytes = " << nResult - sizeof (ICMPheader) - sizeof (IPheader) 
-					<< ", time = " << nRoundTripTime << "ms, TTL = " << (int)ipHdr.byTtl << endl;
-
-				nTotalRoundTripTime = nTotalRoundTripTime + nRoundTripTime;
-
-				if (nMinRoundTripTime == -1)
-				{
-					nMinRoundTripTime = nRoundTripTime;
-					nMaxRoundTripTime = nRoundTripTime;
+				else if (rtt.count()< nMinRoundTripTime){
+					nMinRoundTripTime = rtt.count();;
 				}
-				else if (nRoundTripTime < nMinRoundTripTime)
-				{
-					nMinRoundTripTime = nRoundTripTime;
+				else if (rtt.count()> nMaxRoundTripTime){
+					nMaxRoundTripTime = rtt.count();;
 				}
-				else if (nRoundTripTime > nMaxRoundTripTime)
-				{
-					nMaxRoundTripTime = nRoundTripTime;
-				}
-
 				++nPacketsReceived;
 			}
-			else
-			{
+			else{
 				cout << "The echo reply is not correct!" << endl;
 			}
-
-			delete []pRecvBuffer;
+			delete []RecvBuffer;
 		}
-		else
-		{
+		else{
 			cout << "Request timed out." << endl;
 		}
-
-		delete []pSendBuffer;
-		
-		
-	//}
-	
+		delete []SendBuffer;		
+	}
 }
-
-
-
