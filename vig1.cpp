aa26bda4cc34fa76ffe677fd9247c5eae3a96ad1
alex-and-ria <alex-and-ria@meta.ua>
2017-03-10 20:19:13 +0200

    10.03.2017/20:19

--- vig1.cpp
+++ vig1.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 typedef unsigned short int usi;
 
-bool gff(string& ifn,string& dst/*,string& key*/){
+bool cvig(string& ifn,string& dst/*,string& key*/){
 	ifstream fpi; ofstream fpo;
     fpi.open(ifn.c_str(),ios::binary);
     if(!fpi.is_open()) {cout<<"\nerr: gff no input file"; return 0;}
@@ -40,10 +40,45 @@ bool gff(string& ifn,string& dst/*,string& key*/){
     return 1;
 }
 
+bool decvig(string& ifn,string& dst/*,string& key*/){
+	ifstream fpi; ofstream fpo;
+    fpi.open(ifn.c_str(),ios::binary);
+    if(!fpi.is_open()) {cout<<"\nerr: gff no input file"; return 0;}
+    else{
+    	fpo.open(dst.c_str(),ios::binary);
+    	if(!fpo.is_open()) {cout<<"\nerr: gff no input file"; return 0;}
+    	else{
+    		unsigned char ch='\0'; unsigned long ulcnt=0; bool uc=0;
+			while(1){
+				fpi>>noskipws>>ch;
+				if(fpi.eof()) break;
+				if(((usi)ch<'A')||((usi)ch>'z')||((usi)ch>'Z'&&(usi)ch<'a'))//skip letters that is not in alphabet;
+	    			fpo<<ch;
+	    		else{
+	    			if(isupper(ch)){uc=1; ch=tolower(ch);}
+					else uc=0;
+	    			unsigned char tmpch=(ch-key[ulcnt%(sizeof(key)-1)]+engal)%engal+'a';
+	    			/*cout<<"\nch+key[ulcnt%(sizeof(key)-1)]-2*'a')%engal+'a'="<<
+	    			(unsigned char)ch<<'+'<<key[ulcnt%(sizeof(key)-1)]<<"-2*a%"<<engal<<"+a=\n"<<
+	    			(usi)ch<<'+'<<(usi)key[ulcnt%(sizeof(key)-1)]<<"-2*"<<(usi)'a'<<'%'<<engal<<'+'<<(usi)'a'<<"=\n"<<
+	    			(usi)((ch+key[ulcnt%(sizeof(key)-1)]-2*'a')%engal+'a')<<'='<<
+					(unsigned char)((ch+key[ulcnt%(sizeof(key)-1)]-2*'a')%engal+'a');*/
+	    			if(uc) tmpch=toupper(tmpch);
+	    			fpo<<tmpch;
+	    			ulcnt++;
+	    		}
+			}
+	     fpi.close(); fpo.close();
+		}    
+    }
+    return 1;
+}
+
 int main(){
-	string srs="q.txt",dst="qoup.txt";
+	string srs="q.txt",dst="qoup.txt",dcdf="qres.txt";
 	//string key;	cout<<"enter key\n"; cin>>key; 
 	cout<<"\nkey="<<key;//<<"\nfdt="<<fdt;
-	gff(srs,dst/*,key*/);
+	cvig(srs,dst/*,key*/);
+	decvig(dst,dcdf/*,key*/);
 	return 0;
 }
