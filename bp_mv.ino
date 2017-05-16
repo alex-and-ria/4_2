0485d1c09d6a03acf953858577c424e39cca88ad
alex-and-ria <alex-and-ria@meta.ua>
2017-05-16 18:37:12 +0300

    16.05.2017/18:37

---
+++ bp_mv.ino
@@ -0,0 +1,28 @@
+#define dir 8
+#define stp 9
+#define dly 10
+
+void setup() {
+  pinMode(dir,OUTPUT);
+  pinMode(stp,OUTPUT);
+  //digitalWrite(dir,LOW);
+}
+
+void loop() {
+  digitalWrite(dir,HIGH);
+  delay(dly);
+  for(unsigned int i=0;i<20;i++){
+    digitalWrite(stp, HIGH);
+    delay(dly);
+    digitalWrite(stp, LOW);
+    delay(dly);
+  }
+  digitalWrite(dir,LOW);
+  delay(dly);
+  for(unsigned int i=0;i<20;i++){
+    digitalWrite(stp, HIGH);
+    delay(dly);
+    digitalWrite(stp, LOW);
+    delay(dly);
+  }
+}
